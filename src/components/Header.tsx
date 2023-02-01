c3c987d4581d780557679343314fdcc3151ab0c4
Ilya Zverev <ilya.a.zverev@gmail.com>
2023-02-01 13:29:21 +0400

    12 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+	return <header className="header">
+		<img src="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png" alt="logo"/>
+	</header>;
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+function Profile() {
+	return <div className="content">
+		<div><img
+			src="https://cdn.futura-sciences.com/cdn-cgi/image/width=1920,quality=50,format=auto/sources/images/dossier/773/01-intro-773.jpg"
+			alt="pic"/>
+		</div>
+		<div>
+			<img src="https://blog.hootsuite.com/wp-content/uploads/2020/02/Image-copyright-768x768.png.webp"
+					 alt="pct"/>
+			<div>Description</div>
+		</div>
+		<div>
+			My post
+			<div>
+				New post
+				<div>post1</div>
+				<div>post2</div>
+			</div>
+		</div>
+	</div>;
+
+}
+
+export default Profile;
